3251a929ca5607c912ce8f0a85f95cf4a54bb6d0
iamjayenmthakuri <malla.jayen@gmail.com>
2023-05-06 21:31:58 +0545

    Creating useEffect

--- Reacthook/src/App.jsx
+++ Reacthook/src/App.jsx
@@ -1,5 +1,15 @@
-function App() {
-  return <div>App</div>;
-}
+import { useEffect, useState } from "react";
+
+function App({propA, propB}) {
+  const [count, setCount] = useState(0)
+
+  useEffect(()=>{
+    console.log("useEffect : propA or propB changed")
+  }[propA, propB])
+  return(
+  <div><p>count: {count}</p>
+  <button onClick={handleClick}>Increment Count</button>
+  </div>
+)}
 
 export default App;
