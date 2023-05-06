dc676457ecaab63d0425d96e76d684961748052f
iamjayenmthakuri <malla.jayen@gmail.com>
2023-05-06 21:35:31 +0545

    creating useMemo

--- Reacthook/src/App.jsx
+++ Reacthook/src/App.jsx
@@ -1,15 +1,23 @@
 import { useEffect, useState } from "react";
 
-function App({propA, propB}) {
-  const [count, setCount] = useState(0)
+function App({ propA, propB }) {
+  const [count, setCount] = useState(0);
 
-  useEffect(()=>{
-    console.log("useEffect : propA or propB changed")
-  }[propA, propB])
-  return(
-  <div><p>count: {count}</p>
-  <button onClick={handleClick}>Increment Count</button>
-  </div>
-)}
+  useEffect(() => {
+    console.log("useEffect : propA or propB changed");
+  }, [propA, propB]);
+
+  const memoizedValue = useMemo(() => {
+    console.log(" useMemo : expensive calculation");
+    return propA * 2;
+  }, [propA]);
+
+  return (
+    <div>
+      <p>count: {count}</p>
+      <button onClick={handleClick}>Increment Count</button>
+    </div>
+  );
+}
 
 export default App;
